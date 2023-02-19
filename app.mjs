305670ddd748b96b4736fa07b4b06addb8a0c481
woollyblanket <woollyblanket@users.noreply.github.com>
2023-02-19 03:06:33 +0000

    Update app.mjs
    Add auto to session cookies

--- app.mjs
+++ app.mjs
@@ -75,7 +75,7 @@ app.use(
 
 app.use(
 	session({
-		cookie: { maxAge: 86400000 },
+		cookie: { maxAge: 86400000, secure: "auto" },
 		store: new MemoryStore({
 			checkPeriod: 86400000, // prune expired entries every 24h
 		}),
